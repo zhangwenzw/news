a0e4b4d2df5a89a0cd46b9e782de4695a876a31c
zhangwenzw <604639246@qq.com>
2016-12-06 00:17:02 +0800

    add

--- js/news.js
+++ js/news.js
@@ -1,40 +1,18 @@
-function resize(Origin,type){
-    var widths=document.documentElement.clientWidth;
-    var heights=document.documentElement.clientHeight;
-    var type=type||"x";
-    if(type=="x"){
-       var scale=widths/Origin*100;
-    }else if(type=="y"){
-        var scale=heights/Origin*100;
-    }
-    document.getElementsByTagName("html")[0].style.fontSize=scale+"px";
-}
-   resize(750,"y");
-   $(function(){
-    var mySwiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable :true,
-    })
-})
-   
 $(function(){
-        var width = $(".classify").width();
-        $(".classify").mousedown(function(e){
-            e.preventDefault();
-        })
-        $(".classify").mousemove(function(e){
-            e.preventDefault();
-        });
-        var marginL;
-        touch.on("body","dragstart",".classify-con",function(e){
-            $(".classify-con").css({
-                transition:"none"
-            });
-            marginL = parseInt($(".classify-con").css("marginLeft"))?parseInt($(".classify-con").css("marginLeft")):0;
-        });
-         touch.on("body","drag",".classify-con",function(e){
-            $(".classify-con").css({
-                marginLeft:e.x+marginL
-            })
-        });
+	var mySwiper = new Swiper('.banner', {
+	    pagination: '.swiper-pagination',
+	    paginationClickable :true,
+	})
+	var mySwiper1 = new Swiper('.box', {
+	    scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true
+	})
+    var swiper = new Swiper('.classify .swiper-container', {
+        slidesPerView: 0,
+        spaceBetween: 0,
+        freeMode: true
+    });
 })
---
+++ js/size.js
@@ -0,0 +1,15 @@
+function resize(origin,type){
+    var type=type||"x";
+    if(type=="x"){
+        var width=document.documentElement.clientWidth;
+        var scale=width/origin*100;
+    }else if(type=="y"){
+        var height=document.documentElement.clientHeight;
+        var scale=height/origin*100;
+    }
+    var html=document.getElementsByTagName("html")[0];
+    html.style.fontSize=scale+"px";
+}
+window.onload=function(){
+	resize(1334,"y");
+}
